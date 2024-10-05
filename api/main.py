9a379365ea586325dbe079fc359a60f2f2079f73
AlexFlynnRider <158479256+AlexFlynnRider@users.noreply.github.com>
2024-10-05 15:24:42 -0300

    oie

--- api/main.py
+++ api/main.py
@@ -10,64 +10,89 @@ class SimplexInput(BaseModel):
     c: List[float]
     A: List[List[float]]
     b: List[float]
+    tipo: str = 'max'  # Default 'max' para maximização
 
-# Função Simplex com conversão de tipos
-def simplex(c, A, b):
-    # Certificar-se de que todos os valores estão no formato de float
-    c = np.array(c, dtype=float)
-    A = np.array(A, dtype=float)
-    b = np.array(b, dtype=float)
+# Função Simplex
+def simplex(c, A, b, tipo='max'):
 
-    num_variables = len(c)
-    num_constraints = len(b)
+    print('################################')
+    print('  1° Etapa: Contrucao da Tabela')
+    print('################################ \n')
 
-    # Verificar se o número de linhas de A é igual ao número de elementos em b
-    assert len(A) == num_constraints, "A matriz A deve ter o mesmo número de linhas que o vetor b"
+    # Numero de restrições (linhas) e numero de variaveis (colunas)
+    num_restricao, num_variaveis = A.shape
 
-    # Criar a tabela do simplex
-    tableau = np.zeros((num_constraints + 1, num_variables + num_constraints + 1))
-    tableau[:num_constraints, :num_variables] = A
-    tableau[:num_constraints, num_variables:num_variables + num_constraints] = np.eye(num_constraints)
-    tableau[:num_constraints, -1] = b
-    tableau[-1, :num_variables] = -c
+    # Criar a tabela inicial com as variaveis de folga
+    tabela = np.zeros((num_restricao + 1, num_variaveis + num_restricao + 1))
 
-    # Método Simplex
-    while True:
-        if np.all(tableau[-1, :-1] >= 0):
-            break
-        pivot_col = np.argmin(tableau[-1, :-1])
-        ratios = tableau[:-1, -1] / tableau[:-1, pivot_col]
-        ratios[ratios <= 0] = np.inf
-        pivot_row = np.argmin(ratios)
-        pivot_val = tableau[pivot_row, pivot_col]
-        tableau[pivot_row, :] /= pivot_val
-        for i in range(len(tableau)):
+    # Preencher a tabela com as restricoes
+    tabela[:num_restricao, :num_variaveis] = A
+    tabela[:num_restricao, num_variaveis:num_variaveis + num_restricao] = np.eye(num_restricao)
+    tabela[:num_restricao, -1] = b
+
+    # Preencher a linha da função objetivo
+    if tipo == 'min':
+        tabela[-1, :num_variaveis] = -c
+    else:
+        tabela[-1, :num_variaveis] = c
+
+    # Função para verificar se o critério de otimalidade foi atingido
+    def is_optimal():
+        if tipo == 'min':
+            return np.all(tabela[-1, :-1] >= 0)
+        else:
+            return np.all(tabela[-1, :-1] <= 0)
+
+    # Funcao para escolher a variavel de entrada
+    def get_pivot_column():
+        if tipo == 'min':
+            return np.argmin(tabela[-1, :-1])
+        else:
+            return np.argmax(tabela[-1, :-1])
+
+    # Funcao para escolher a variavel de saida
+    def get_pivot_row(pivot_col):
+        ratios = tabela[:-1, -1] / tabela[:-1, pivot_col]
+        valid_ratios = np.where(ratios > 0, ratios, np.inf)
+        return np.argmin(valid_ratios)
+
+    # Funcao de pivotagem
+    def pivot(pivot_row, pivot_col):
+        tabela[pivot_row, :] /= tabela[pivot_row, pivot_col]
+        for i in range(len(tabela)):
             if i != pivot_row:
-                tableau[i, :] -= tableau[i, pivot_col] * tableau[pivot_row, :]
+                tabela[i, :] -= tabela[i, pivot_col] * tabela[pivot_row, :]
 
-    solution = np.zeros(num_variables)
-    for i in range(num_constraints):
-        basic_var = np.where(tableau[i, :num_variables] == 1)[0]
+    # Loop ate encontrar a solução otima
+    interacao = 1
+    while not is_optimal():
+        print(f'Interação {interacao}:\n')
+        pivot_col = get_pivot_column()
+        pivot_row = get_pivot_row(pivot_col)
+        pivot(pivot_row, pivot_col)
+        interacao += 1
+    print('Sim, terminou!!')
+
+    # A solucao otima sera encontrada nas colunas das variaveis de folga
+    solution = np.zeros(num_variaveis)
+    for i in range(num_restricao):
+        basic_var = np.where(np.abs(tabela[i, :num_variaveis]) == 1)[0]
         if len(basic_var) == 1:
-            solution[basic_var[0]] = tableau[i, -1]
+            solution[basic_var[0]] = tabela[i, -1]
 
-    z_opt = tableau[-1, -1]
-    return solution, z_opt
+    return solution, tabela[-1, -1]
 
 # Rota para resolver o problema com Simplex
 @app.post("/solve_simplex/")
 def solve_simplex(input_data: SimplexInput):
     try:
-        # Printar os dados recebidos para depuração
-        print(f"Recebido c: {input_data.c}")
-        print(f"Recebido A: {input_data.A}")
-        print(f"Recebido b: {input_data.b}")
-
-        solution, z_opt = simplex(input_data.c, input_data.A, input_data.b)
+        c = np.array(input_data.c)
+        A = np.array(input_data.A)
+        b = np.array(input_data.b)
+        solution, z_opt = simplex(c, A, b, tipo=input_data.tipo)
         return {"solution": solution.tolist(), "z_opt": z_opt}
     except AssertionError as e:
         raise HTTPException(status_code=400, detail=str(e))
     except Exception as e:
-        # Printar erro para depuração
         print(f"Erro ao resolver o problema: {str(e)}")
         raise HTTPException(status_code=500, detail="Erro interno ao resolver o problema.")
